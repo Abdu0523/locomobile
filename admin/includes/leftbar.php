a0834a0324d56c8d2fa9e50527a39ade00df2698
Abdu <ndiayeabdoundour@proton.me>
2023-11-11 09:53:15 +0000

    locomobile initialised

---
+++ admin/includes/leftbar.php
@@ -0,0 +1,31 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Principal</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+			
+<li><a href="#"><i class="fa fa-files-o"></i> Marques </a>
+<ul>
+<li><a href="create-brand.php">Créer Marque</a></li>
+<li><a href="manage-brands.php">Gérer les Marques</a></li>
+</ul>
+</li>
+
+<li><a href="#"><i class="fa fa-sitemap"></i> Vehicules</a>
+					<ul>
+						<li><a href="post-avehical.php">Poster un Vehicule</a></li>
+						<li><a href="manage-vehicles.php">Gérer Vehicules</a></li>
+					</ul>
+				</li>
+				<li><a href="manage-bookings.php"><i class="fa fa-users"></i> Gérer Réservation</a></li>
+
+				<li><a href="testimonials.php"><i class="fa fa-table"></i> Gérer Témoignages</a></li>
+				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop""></i> Gérer requete de Contact</a></li>
+				<li><a href="reg-users.php"><i class="fa fa-users"></i> Gérer Utilisateur</a></li>
+			<li><a href="manage-pages.php"><i class="fa fa-files-o"></i> Gérer les Pages</a></li>
+			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Mis à jour Info Contact </a></li>
+
+			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Gérer Abonnés</a></li>
+
+			</ul>
+		</nav>
---
+++ includes/sidebar.php
@@ -0,0 +1,11 @@
+<div class="profile_nav">
+          <ul>
+            <li><a href="profile.php">Profile </a></li>
+              <li><a href="update-password.php">Mon mot de pass</a></li>
+            <li><a href="my-booking.php">Mes Reservations</a></li>
+            <li><a href="post-testimonial.php">Poster un Temoignage</a></li>
+               <li><a href="my-testimonials.php">Ma Temoignage</a></li>
+            <li><a href="logout.php">Se Deconnecter</a></li>
+          </ul>
+        </div>
+      </div>
